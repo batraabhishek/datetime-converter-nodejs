1c7f5f7a93eafc6486b823a77956f5b625e2d82c
Abhishek Batra <abhishekbatra1993@gmail.com>
2015-05-16 16:07:36 +0530

    Added time diff functions

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.exports = {
 	isoString: function(date) {
-		if(date) {
+		if (date) {
 			dDate = new Date(date);
 			return dDate.toISOString();
-		}
-		else {
+		} else {
 			date = new Date();
 			return date.toISOString();
 		}
@@ -12,11 +11,48 @@ module.exports = {
 	},
 
 	dateString: function(isoDate) {
-		if(isoDate) {
+		if (isoDate) {
 			return new Date(isoDate);
-		}
-		else {
+		} else {
 			return new Date();
+		}
+
+	},
+
+	isoTimeDiff: function(isoDate1, isoDate2) {
+
+		if (!isoDate1) {
+			return 0;
+		}
+
+		var seconds1, seconds2;
+		if (!isoDate2) {
+			isoDate2 = new Date();
+			seconds1 = new Date(isoDate1).getTime() / 1000;
+			seconds2 = isoDate2.getTime() / 1000;
+		} else {
+			seconds1 = new Date(isoDate1).getTime() / 1000;
+			seconds2 = new Date(isoDate2).getTime() / 1000;
+		}
+
+		return (seconds1 - seconds2);
+
+	},
+
+	timeDiff: function(time1, time2) {
+		var seconds1, seconds2;
+		if (time1) {
+			return 0;
+		} else {
+			if (time2) {
+				seconds1 = new Date(time1).getTime() / 1000;
+				seconds2 = new Date(time2).getTime() / 1000;
+
+			} else {
+				seconds1 = new Date(time1).getTime() / 1000;
+				seconds2 = new Date().getTime() / 1000;
+
+			}
 		}
 
 	}
